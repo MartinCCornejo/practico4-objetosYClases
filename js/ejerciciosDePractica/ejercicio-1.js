f910e5a68d7111206d5a07c167376b96b728a71f
Martin Cristian Cornejo <martincckd130@gmail.com>
2023-11-10 16:25:45 -0300

    Ejercicios de practica 1 y 2 finalizado

--- js/ejerciciosDePractica/ejercicio-1.js
+++ js/ejerciciosDePractica/ejercicio-1.js
@@ -2,12 +2,12 @@
 // Gestión de inventario en una tienda: Tienes dos arrays, uno que representa los productos en stock y otro que almacena los precios de los productos. Debes crear funciones que permitan agregar nuevos productos al inventario, eliminar productos agotados y calcular el valor total del inventario.
 
 const productosCelulares = [
-    'Motorola G23 128GB (AGOTADO)',
+    'Motorola G23 128GB - (AGOTADO)',
     'Xiaomi Redmi 10C 128GB',
     'ZTE Blade A33 Plus 32GB',
-    'Motorola G13 128GB (AGOTADO)',
+    'Motorola G13 128GB - (AGOTADO)',
     'Samsung A04s 128GB',
-    'Samsung Galaxy A14 128GB (AGOTADO)',
+    'Samsung Galaxy A14 128GB - (AGOTADO)',
     'Motorola G72 128GB',
     'Xiaomi Redmi Note 11 128GB',
     'iPhone 13 Pro 256GB'
@@ -26,14 +26,14 @@ const preciosCelulares = [
 ]
 
 // Ver la lista de los productos en pantalla 
-const verProductos = (titulo) => {
-    document.write(`<h2>${titulo} (Total: ${productosCelulares.length} productos)</h2>`);
+const verProductos = (array,titulo) => {
+    document.write(`<h2>${titulo}<br> (Total: ${array.length} productos)</h2>`);
     document.write('<ul>');
-    productosCelulares.map((elemento) => document.write(`<li>${elemento}</li>`));
+    array.map((elemento) => document.write(`<li>${elemento}</li>`));
     document.write('</ul>');
 }
 
-verProductos('Lista de productos original');
+verProductos(productosCelulares,'Lista de productos original');
 
 
 // Función para agregar nuevos productos 
@@ -43,31 +43,28 @@ const agregarproducto = (producto,precio) => {
 }
 
 agregarproducto('Huawei Mate 20 Lite',50000);
-verProductos('Lista de productos con el nuevo producto agregado');
+verProductos(productosCelulares,'Lista de productos con el nuevo producto agregado');
 
 
 // Función para eliminar productos agotados
 const eliminarProductosAgotados = (producto) => {
-    productosCelulares.splice(producto,1);
-    preciosCelulares.splice(producto,1);
+    const productosDisponibles = producto.filter((elemento) => !elemento.includes('AGOTADO'));  /* Si el producto NO inluye 'AGOTADO', lo agregamos al nuevo array*/
+    return productosDisponibles;
 }
 
-// Indicamos como argumento que posición del producto en el array se eliminará
-eliminarProductosAgotados(0);
-eliminarProductosAgotados(2);
-eliminarProductosAgotados(3);
-verProductos('Lista de productos actualizada con los productos agotados eliminados');
+let productosDisponibles = eliminarProductosAgotados(productosCelulares);
+verProductos(productosDisponibles,'Lista de productos actualizada con los productos agotados eliminados');
 
 
 // Calculamos el valor total del inventario 
-const valorTotalDelInventario = () => {
+const valorTotalDelInventario = (precios) => {
     let total = 0;
 
-    for (let i = 0; i < preciosCelulares.length; i++) {
-        total += preciosCelulares[i];
+    for (let i = 0; i < precios.length; i++) {
+        total += precios[i];
     }
-    
+
     return total;
 }
 
-document.write(`<h3>El valor total del inventario es: $${valorTotalDelInventario()}</h3<`);
+document.write(`<hr><h3>El valor total del inventario es: $${valorTotalDelInventario(preciosCelulares)}</h3<`);
